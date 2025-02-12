cff517dd54ba7910fe412e7cd3a0bb1bfc89f148
maybetree <maybetree48@proton.me>
2025-02-12 10:41:27 +0000

    Update for raimad 1.2.1

--- src/rai_smiley/smiley.py
+++ src/rai_smiley/smiley.py
@@ -39,9 +39,9 @@ class Smiley(rai.Compo):
     def _make(self, eye_size: float = 5, happiness: float = 8):
         face = rai.Circle(50).proxy().map('face')
         eye_l = rai.Circle(eye_size).proxy().map('eyes')
-        eye_r = eye_l.copy()
+        eye_r = eye_l.shallow_copy()
 
-        smile = rai.AnSec(
+        smile = rai.AnSec.from_auto(
             thetamid=radians(-90),
             dtheta=radians(abs(happiness) * 10),
             r1=40,
